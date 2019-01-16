5f47158e25f613cc19d45f8dbc70d85efe9a148e
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-16 14:15:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name){
-  array.push(kittens)
+  array.push(kittens);
+}
+
+function destructivelyPrependKitten(name){
+  array.unshift(kittens);
 }
 
 function 
