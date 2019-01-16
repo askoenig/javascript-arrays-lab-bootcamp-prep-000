25058dde598e529069c4e5162478b85801f9278d
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-16 15:18:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,11 +26,12 @@ function prependKitten(name){
   return [name, ...kittens];
 }
 
+
 function removeLastKitten(){
-  kittens.slice(0, kittens.length - 1);
-  var otherKittens =
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten(){
  return kittens.slice(1);
 }
+
